94a603600eee38769e9370235601ba85346813de
Ayame <0819aym001@gmail.com>
2024-10-18 23:56:50 +0900

    백준 4-2

---
+++ src/study/백준/Stage4/No2_10871.java
@@ -0,0 +1,25 @@
+package study.백준.Stage4;
+
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class No2_10871 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int max = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < length; i++) {
+            int num  = sc.nextInt();
+            if (num < max) {
+                list.add(num);
+            }
+        }
+        for (int k : list) {
+            System.out.print(k + " ");
+        }
+    }
+}
