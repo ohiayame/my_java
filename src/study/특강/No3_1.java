2efbb3c55f269201c29d8e824ab91d6ffecaaded
Ayame <166793391+ohiayame@users.noreply.github.com>
2024-10-28 22:08:31 +0900

    1028

--- src/study/특강/No3_1.java
+++ src/study/특강/No3_1.java
@@ -1,19 +1,29 @@
 package study.특강;
 
+import java.util.Scanner;
+
 public class No3_1 {
     public static void main(String[] args) {
-        int age = 20;
-        String eventCode = "E1";
-        int reservationDate = 30;
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("나이를 입력하세요: ");
+        int age = sc.nextInt();
+
+        System.out.print("예약하려는 이벤트 코드를 입력하세요: ");
+        String eventCode = sc.next();
+
+        System.out.print("원하는 예약 날짜를 입력하세요: ");
+        int reservationDate = sc.nextInt();
+        // 예약 가능한지
         boolean isReservation = true;
+        // 안될 때 문장
         String msg = "";
-        
+        // 범위 외의 날짜를 입력하면 종료
         if (reservationDate < 1 && reservationDate > 30) {
-            isReservation = false;
-            msg = "잘못된 입력입니다. 프로그램을 종료합니다.";
+            System.out.print("잘못된 입력입니다. 프로그램을 종료합니다.");
+            return;
         }
-        
+        // 이벤트 코드에 따라 오류 메시지를 설정
         switch (eventCode){
             case "E1":
                 if (age < 18) {
